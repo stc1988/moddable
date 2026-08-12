3bf3d5c1e4953b2fc7d6c3377e6cd6f78a631e06
Michael Kellner <mkellner@intlweb.com>
2026-08-12 00:43:15 -0700

    419 backlight

---
+++ modules/drivers/peripherals/backlight/backlight.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2026  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+class Backlight {
+	#io;
+	#invert;
+	#brightness = 1.0;
+
+	constructor(options) {
+		if (!options.io)
+			throw new Error("no Backlight io");
+		this.#io = new options.io(options);
+		this.#invert = options.invert ?? false;
+	}
+	close() {
+		this.#io?.close();
+		this.#io = undefined;
+	}
+	set brightness(value) {
+		if (value <= 0)
+			value = 0;
+		else if (value >= 1)
+			value = 1023;
+		else
+			value *= 1023;
+		if (this.#invert)
+			value = 1023 - value;
+		this.#io.write(value);
+		this.#brightness = value / 1024;
+	}
+	get brightness() {
+		return this.#brightness;
+	}
+	write(value) {
+		this.brightness = value / 100;
+	}
+}
+
+export default Backlight;
