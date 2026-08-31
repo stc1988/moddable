64ae4f07cc1f9c412bc89f721a689d07b2bdee6f
Peter Hoddie <peter@moddable.com>
2026-08-30 21:53:37 -0700

    bring up-to-date

--- build/devices/esp32/targets/moddable_two_io/host/provider.js
+++ build/devices/esp32/targets/moddable_two_io/host/provider.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023  Moddable Tech, Inc.
+ * Copyright (c) 2021-2026  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  *
@@ -23,96 +23,16 @@ import Digital from "embedded:io/digital";
 import DigitalBank from "embedded:io/digitalbank";
 import I2C from "embedded:io/i2c";
 import PulseCount from "embedded:io/pulsecount";
+import PulseWidth from "embedded:io/pulsewidth";
 import PWM from "embedded:io/pwm";
 import Serial from "embedded:io/serial";
 import SMBus from "embedded:io/smbus";
 import SPI from "embedded:io/spi";
+
+import Backlight from "backlight";
+import Button from "button";
+import LED from "led/pwm";
 import Touch from "embedded:sensor/Touch/FT6x06";
-import PulseWidth from "embedded:io/pulsewidth";
-
-class Backlight {
-	#io;
-
-	constructor(options) {
-		this.#io = new PWM(options);
-	}
-	close() {
-		this.#io?.close();
-		this.#io = undefined;
-	}
-	set brightness(value) {
-		value = 1 - value;		// PWM is inverted
-		if (value <= 0)
-			value = 0;
-		else if (value >= 1)
-			value = 1023;
-		else
-			value *= 1023;
-		this.#io.write(value);
-	}
-	write(value) {		// compatibility
-		this.brightness = value / 100;
-	}
-}
-
-class LED {
-	#io;
-
-	constructor(options) {
-		options = {...options};
-		if (options.target)
-			this.target = options.target;
-
-		this.#io = new (options.io)(options);
-		if (options.invert)
-			this.#io.invert = true;
-		this.on = 0;
-	}
-	close() {
-		this.#io?.close();
-		this.#io = undefined;
-	}
-	set on(value) {
-		const range = (1 << this.#io.resolution) - 1;
-		this.#io.value = Number(value);
-		value = (this.#io.value * range) | 0;
-		this.#io.write(this.#io.invert ? range - value : value);
-	}
-	get on() {
-		return this.#io.value;
-	}
-}
-
-class Button {
-	#io;
-	#onPush;
-
-	constructor(options) {
-		options = {...options};
-		if (options.onReadable || options.onWritable || options.onError)
-			throw new Error;
-
-		if (options.target)
-			this.target = options.target;
-
-		const Digital = options.io;
-		if (options.onPush) {
-			this.#onPush = options.onPush; 
-			options.onReadable = () => this.#onPush();
-			options.edge = Digital.Rising | Digital.Falling;
-		}
-
-		this.#io = new Digital(options);
-		this.#io.pressed = options.invert ? 0 : 1;
-	}
-	close() {
-		this.#io?.close();
-		this.#io = undefined;
-	}
-	get pressed() {
-		return (this.#io.read() === this.#io.pressed) ? 1 : 0;
-	}
-}
 
 const device = {
 	I2C: {
@@ -153,36 +73,25 @@ const device = {
 		displayDC: 2,
 		displaySelect: 15
 	},
-	sensor: {
-		Touch: class {
-			constructor(options) {
-				const result = new Touch({
-					sensor: {
-						...device.I2C.default,
-						io: device.io.SMBus
-					},
-					...options
-				});
-				result.configure({threshold: 20});
-				return result;
-			}
-		}
-	},
 	peripheral: {
 		Backlight: class {
 			constructor() {
-				return new Backlight({pin: device.pin.backlight});
+				return new Backlight({
+					io: device.io.PWM,
+					pin: device.pin.backlight,
+					invert: true
+				});
 			}
 		},
 		button: {
-			Flash: class {
+			Default: class {
 				constructor(options) {
 					return new Button({
 						...options,
-						io: Digital,
+						io: device.io.Digital,
 						pin: device.pin.button,
 						mode: Digital.InputPullUp,
-						invert: true
+						activeLow: true
 					});
 				}
 			}
@@ -195,6 +104,21 @@ const device = {
 						pin: device.pin.led
 					});
 				}
+			}
+		}
+	},
+	sensor: {
+		Touch: class {
+			constructor(options) {
+				const result = new Touch({
+					sensor: {
+						...device.I2C.default,
+						io: device.io.SMBus
+					},
+					...options
+				});
+				result.configure({threshold: 20});
+				return result;
 			}
 		}
 	}
