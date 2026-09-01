287b26b8a29c726aeb4d54c445aaedfac3ff23b5
Peter Hoddie <peter@moddable.com>
2026-09-01 15:10:53 -0700

    sync test with implementation

--- tests/xs/issues/__680.js
+++ tests/xs/issues/__680.js
@@ -1,14 +1,17 @@
 /*---
 description: https://github.com/Moddable-OpenSource/moddable/issues/680
+info: |
+  A scripted exec may report a match extending past the end of the subject
+  string. GetSubstitution takes $' as the substring of str from
+  position + matchLength, which is empty in that case, and @@replace appends
+  nothing after the replacement. Neither throws.
 flags: [onlyStrict]
-negative:
-  type: RangeError
 ---*/
 
 let evil1 = new RegExp;
 evil1.exec = () => ({ 0: '1234567', length: 1, index: 0 });
-'abc'.replace(evil1, `$'`);
+assert.sameValue('abc'.replace(evil1, `$'`), "", "match longer than the subject");
 
 let evil2 = new RegExp;
 evil2.exec = () => ({ 0: 'x', length: 1, index: 3 });
-'abc'.replace(evil2, `$'`);
+assert.sameValue('abc'.replace(evil2, `$'`), "abc", "match starting at the end of the subject");
